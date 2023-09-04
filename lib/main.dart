ee1102bb98a1365c99c63b6cfe106a782b5394f2
mahahassan25 <119357314+mahahassan25@users.noreply.github.com>
2023-09-04 14:42:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
           ],
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
-            home: signupScreen()
+            home: signinScreen()
           ));
     }
   }
